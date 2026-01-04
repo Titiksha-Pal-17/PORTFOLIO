ad78c8a2089650e6894d9099d3800d2e46bdeaf0
Titiksha-Pal-17 <titikshapal765@gmail.com>
2026-01-04 19:45:40 +0530

    Fix formatting in OrbitingCircles component

--- src/components/OrbitingCircles.jsx
+++ src/components/OrbitingCircles.jsx
@@ -41,8 +41,7 @@ export function OrbitingCircles({
               "--icon-size": `${iconSize}px`,
             }}
             className={twMerge(
-              `absolute flex size-
-              [var(--icon-size)] transform-gpu animate-orbit items-center justify-center rounded-full ${
+              `absolute flex size-[var(--icon-size)] transform-gpu animate-orbit items-center justify-center rounded-full ${
                 reverse ? "[animation-direction:reverse]" : ""
               }`,
               className
@@ -56,4 +55,3 @@ export function OrbitingCircles({
     </>
   );
 }
-          
